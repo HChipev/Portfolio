33b672cd45a433d678f21feffa75981edaf0db5d
Hristo Chipev <107315850+HChipev@users.noreply.github.com>
2023-07-27 09:19:21 +0300

    Add metadata

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Contact from "./Contact";
 import Footer from "./Footer";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { ToastContainer } from "react-toastify";
+import { Helmet } from "react-helmet";
 
 // import your icons
 import { fab } from "@fortawesome/free-brands-svg-icons";
@@ -40,6 +41,36 @@ function App() {
 
   return (
     <div className="flex flex-col items-center bg-lightGray h-screen">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Hristo Chipev</title>
+        <meta
+          name="description"
+          content="Hi there! I'm Hristo Chipev, a passionate web developer with expertise in React, JavaScript, C#, .NET, and SQL. Check out my portfolio to see my projects and achievements in web development."
+        />
+        <meta
+          name="keywords"
+          content="portfolio, web development, React, JavaScript, projects, skills, achievements, frontend, backend, C#, .NET, SQL"
+        />
+        <meta name="author" content="Hristo Chipev" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content="Hristo Chipev" />
+        <meta
+          property="og:description"
+          content="Hi there! I'm Hristo Chipev, a passionate web developer with expertise in React, JavaScript, C#, .NET, and SQL. Check out my portfolio to see my projects and achievements in web development."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://hristo.ch/" />
+        <meta property="og:image" content="http://hristo.ch/logo.png" />
+        <meta property="og:image:alt" content="Hristo Chipev logo" />
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:title" content="Hristo Chipev" />
+        <meta
+          property="twitter:description"
+          content="Hi there! I'm Hristo Chipev, a passionate web developer with expertise in React, JavaScript, C#, .NET, and SQL. Check out my portfolio to see my projects and achievements in web development."
+        />
+        <meta property="twitter:image" content="http://hristo.ch/logo.png" />
+      </Helmet>
       <Navbar
         aboutRef={scrollToAbout}
         portfolioRef={scrollToPortfolio}
